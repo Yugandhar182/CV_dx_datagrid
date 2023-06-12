ce5d72a769309cf62cefc54c1bd054771e8be0f5
Yugandhar182 <yugandhar@recruitly.io>
2023-06-12 20:20:11 +0530

    .

--- src/App.svelte
+++ src/App.svelte
@@ -1,96 +1,106 @@
 <script>
-	import { onMount } from "svelte";
-	import "bootstrap/dist/css/bootstrap.min.css";
-	import DevExpress from "devextreme";
+  import { onMount } from "svelte";
+  import "bootstrap/dist/css/bootstrap.min.css";
+  import DevExpress from "devextreme";
 
-	let jsonData = [];
-	let gridData = [];
+  let jsonData = [];
+  let gridData = [];
 
-	onMount(async () => {
-	  const response = await fetch(
-		"https://api.recruitly.io/api/candidate?apiKey=TEST9349C0221517DA4942E39B5DF18C68CDA154"
-	  );
-	  const responseData = await response.json();
-	  jsonData = responseData.data;
+  onMount(async () => {
+    const response = await fetch(
+      "https://api.recruitly.io/api/candidate?apiKey=TEST9349C0221517DA4942E39B5DF18C68CDA154"
+    );
+    const responseData = await response.json();
+    jsonData = responseData.data;
 
-	  gridData = jsonData.map((item) => ({
-		id: item.id,
-		firstName: item.firstName,
-		surname: item.surname,
-		email: item.email,
-		mobile: item.mobile,
-	  }));
+    gridData = jsonData.map((item) => ({
+      id: item.id,
+      firstName: item.firstName,
+      surname: item.surname,
+      email: item.email,
+      mobile: item.mobile,
+    }));
 
-	  const columns = [
-		{ dataField: "id", caption: "ID", width: 250 },
-		{ dataField: "firstName", caption: "Full Name", width: 200 },
-		{ dataField: "surname", caption: "Surname", width: 200 },
-		{ dataField: "email", caption: "Email", width: 200 },
-		{ dataField: "mobile", caption: "Mobile", width: 150 },
-		{
-		  caption: "Actions",
-		  cellTemplate: function (container, options) {
-			const link = document.createElement("a");
-			link.href = `https://api.recruitly.io/api/candidatecv/${options.data.id}?apiKey=TEST27306FA00E70A0F94569923CD689CA9BE6CA`;
-			link.target = "_blank";
-			link.innerText = "View CV";
-			link.addEventListener("click", async (event) => {
-			  event.preventDefault();
-			  const cvResponse = await fetch(link.href);
-			  if (cvResponse.ok) {
-				const cvBlob = await cvResponse.blob();
-				const cvUrl = URL.createObjectURL(cvBlob);
-				window.open(cvUrl, "_blank");
-			  } else {
-				alert("CV file not found.");
-			  }
-			});
-			container.appendChild(link);
-		  },
-		  width: 150,
-		},
-		// Add other columns as needed
-	  ];
+    const columns = [
+      { dataField: "id", caption: "ID", width: 250 },
+      { dataField: "firstName", caption: "Full Name", width: 200 },
+      { dataField: "surname", caption: "Surname", width: 200 },
+      { dataField: "email", caption: "Email", width: 200 },
+      { dataField: "mobile", caption: "Mobile", width: 150 },
+      {
+        caption: "Actions",
+        cellTemplate: function (container, options) {
+          const link = document.createElement("a");
+          link.href = `https://api.recruitly.io/api/candidatecv/${options.data.id}?apiKey=TEST27306FA00E70A0F94569923CD689CA9BE6CA`;
+          link.target = "_blank";
+          link.innerText = "View CV";
+          link.addEventListener("click", async (event) => {
+            event.preventDefault();
+            const cvResponse = await fetch(link.href);
+            if (cvResponse.ok) {
+              const cvData = await cvResponse.json();
+              const cvHtml = cvData.html;
+              if (cvHtml) {
+                const cvWindow = window.open("", "_blank");
+                cvWindow.document.write(cvHtml);
+                cvWindow.document.close();
+              } else {
+                alert("CV file not found.");
+              }
+            } else {
+              alert("Failed to fetch CV file.");
+            }
+          });
+          container.appendChild(link);
+        },
+        width: 150,
+      },
+      // Add other columns as needed
+    ];
 
-	  const dataGrid = new DevExpress.ui.dxDataGrid(document.getElementById("dataGrid"), {
-		dataSource: gridData,
-		columns: columns,
-		showBorders: true,
-		filterRow: {
-		  visible: true,
-		},
-		editing: {
-		  allowDeleting: true,
-		  allowAdding: true,
-		  allowUpdating: true,
-		  mode: "popup",
-		  form: {
-			labelLocation: "top",
-		  },
-		  popup: {
-			showTitle: true,
-			title: "Row in the editing state",
-		  },
-		  texts: {
-			saveRowChanges: "Save",
-			cancelRowChanges: "Cancel",
-			deleteRow: "Delete",
-			confirmDeleteMessage: "Are you sure you want to delete this record?",
-		  },
-		},
-		paging: {
-		  pageSize: 10,
-		},
+    const dataGrid = new DevExpress.ui.dxDataGrid(
+      document.getElementById("dataGrid"),
+      {
+        dataSource: gridData,
+        columns: columns,
+        showBorders: true,
+        filterRow: {
+          visible: true,
+        },
+        editing: {
+          allowDeleting: true,
+          allowAdding: true,
+          allowUpdating: true,
+          mode: "popup",
+          form: {
+            labelLocation: "top",
+          },
+          popup: {
+            showTitle: true,
+            title: "Row in the editing state",
+          },
+          texts: {
+            saveRowChanges: "Save",
+            cancelRowChanges: "Cancel",
+            deleteRow: "Delete",
+            confirmDeleteMessage:
+              "Are you sure you want to delete this record?",
+          },
+        },
+        paging: {
+          pageSize: 10,
+        },
 
-		onInitialized: () => {},
-	  });
-	});
+        onInitialized: () => {},
+      }
+    );
+  });
 </script>
 
 <style>
-	#dataGrid {
-	  height: 400px;
-	}
+  #dataGrid {
+    height: 400px;
+  }
 </style>
 
 <h1 style="color:blue;">Job Candidate Details</h1>
