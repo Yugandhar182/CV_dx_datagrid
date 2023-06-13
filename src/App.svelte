530f523bb72b395a618214992b6462d24d0c6c4d
Yugandhar182 <yugandhar@recruitly.io>
2023-06-13 17:44:17 +0530

    .

--- src/App.svelte
+++ src/App.svelte
@@ -5,105 +5,129 @@
 
   let jsonData = [];
   let gridData = [];
+  let showPopup = false;
+  let pdfUrl = "";
 
   onMount(async () => {
-    try {
-      const response = await fetch(
-        "https://api.recruitly.io/api/candidate?apiKey=TEST45684CB2A93F41FC40869DC739BD4D126D77"
-      );
+    const response = await fetch(
+      "https://api.recruitly.io/api/candidate?apiKey=TEST9349C0221517DA4942E39B5DF18C68CDA154"
+    );
+    const responseData = await response.json();
+    jsonData = responseData.data;
 
-      if (response.ok) {
-        const responseData = await response.json();
-        jsonData = responseData.data;
+    gridData = jsonData.map((item) => ({
+      id: item.id,
+      firstName: item.firstName,
+      surname: item.surname,
+      email: item.email,
+      mobile: item.mobile,
+    }));
 
-        gridData = jsonData.map((item) => {
-          
-          return {
-            id: item.id,
-            firstName: item.firstName,
-            surname: item.surname,
-            email: item.email,
-            mobile: item.mobile,
-           
-          };
-        });
+    const columns = [
+      { dataField: "id", caption: "ID", width: 250 },
+      { dataField: "firstName", caption: "Full Name", width: 200 },
+      { dataField: "surname", caption: "Surname", width: 200 },
+      { dataField: "email", caption: "Email", width: 200 },
+      { dataField: "mobile", caption: "Mobile", width: 150 },
+      {
+        caption: "Actions",
+        cellTemplate: function (container, options) {
+          const link = document.createElement("a");
+          link.href = `https://api.recruitly.io/api/candidatecv/${options.data.id}?apiKey=TEST27306FA00E70A0F94569923CD689CA9BE6CA`;
+          link.target = "_blank";
+          link.innerText = "View CV";
+          link.addEventListener("click", async (event) => {
+            event.preventDefault();
+            const cvResponse = await fetch(link.href);
+            if (cvResponse.ok) {
+              const blob = await cvResponse.blob();
+              const url = URL.createObjectURL(blob);
+              pdfUrl = url;
+              showPopup = true;
+            } else {
+              alert("Failed to fetch CV file.");
+            }
+          });
+          container.appendChild(link);
+        },
+        width: 150,
+      },
+      // Add other columns as needed
+    ];
 
-        const columns = [
-          { dataField: "id", caption: "ID", width: 250 },
-          { dataField: "firstName", caption: "Full Name", width: 200 },
-          { dataField: "surname", caption: "Surname", width: 200 },
-          { dataField: "email", caption: "Email", width: 200 },
-          { dataField: "mobile", caption: "Mobile", width: 150 },
-          // Add the file button column
-          {
-            caption: "Actions",
-            width: 150,
-            cellTemplate: function (container, options) {
-              console.log("Options Data:", options.data);
-              const downloadButton = document.createElement("a");
-              downloadButton.className = "btn btn-success btn-sm";
-              downloadButton.textContent = "Download";
-              downloadButton.addEventListener("click", () => {
-                downloadCV(options.data.cloudFileId);
-              });
-              container.appendChild(downloadButton);
-            },
+    const dataGrid = new DevExpress.ui.dxDataGrid(
+      document.getElementById("dataGrid"),
+      {
+        dataSource: gridData,
+        columns: columns,
+        showBorders: true,
+        filterRow: {
+          visible: true,
+        },
+        editing: {
+          allowDeleting: true,
+          allowAdding: true,
+          allowUpdating: true,
+          mode: "popup",
+          form: {
+            labelLocation: "top",
           },
-          // Define other columns as needed
-        ];
+          popup: {
+            showTitle: true,
+            title: "Row in the editing state",
+          },
+          texts: {
+            saveRowChanges: "Save",
+            cancelRowChanges: "Cancel",
+            deleteRow: "Delete",
+            confirmDeleteMessage:
+              "Are you sure you want to delete this record?",
+          },
+        },
+        paging: {
+          pageSize: 10,
+        },
 
-        const dataGrid = new DevExpress.ui.dxDataGrid(
-          document.getElementById("dataGrid"),
-          {
-            dataSource: gridData,
-            columns: columns,
-            showBorders: true,
-            filterRow: {
-              visible: true,
-            },
-            paging: {
-              pageSize: 10,
-            },
-          }
-        );
-      } else {
-        console.error("Failed to fetch candidate data.");
+        onInitialized: () => {},
       }
-    } catch (error) {
-      console.error("Error while fetching candidate data:", error);
-    }
+    );
   });
-
-  async function downloadCV(cloudFileId) {
-    console.log("cvid:", cloudFileId);
-    try {
-      const response = await fetch(
-        `https://api.recruitly.io/api/cloudfile/download?cloudFileId=${cloudFileId}&apiKey=TEST45684CB2A93F41FC40869DC739BD4D126D77`
-      );
-
-      if (response.ok) {
-        const blob = await response.blob();
-        const url = URL.createObjectURL(blob);
-        const link = document.createElement("a");
-        link.href = url;
-        link.download = "CV.txt"; // Replace with the desired file name
-        link.click();
-        URL.revokeObjectURL(url);
-      } else {
-        console.error("Failed to download the file.");
-      }
-    } catch (error) {
-      console.error("Error while downloading the file:", error);
-    }
-  }
 </script>
 
 <style>
   #dataGrid {
     height: 400px;
   }
+  
+  .popup {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 600px;
+    height: 800px;
+    background-color: #fff;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    padding: 20px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+  }
+  
+  .popup-close {
+    position: absolute;
+    top: 5px;
+    right: 5px;
+    cursor: pointer;
+  }
 </style>
 
 <h1 style="color:blue;">Job Candidate Details</h1>
 
 <div id="dataGrid"></div>
+
+{#if showPopup}
+  <div class="popup">
+    <div class="popup-close" on:click={() => { showPopup = false; pdfUrl = ""; }}>Close</div>
+    <iframe src="{pdfUrl}" width="100%" height="100%" frameborder="0"></iframe>
+  </div>
+{/if}
