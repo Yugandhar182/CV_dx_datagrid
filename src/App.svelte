ec5ba53bed0fdabf6f6e02e7500396ed758eee39
Yugandhar182 <yugandhar@recruitly.io>
2023-06-13 12:15:27 +0530

    .

--- src/App.svelte
+++ src/App.svelte
@@ -37,7 +37,7 @@
           downloadButton.className = "btn btn-success btn-sm";
           downloadButton.textContent = "Download";
           downloadButton.addEventListener("click", () => {
-            downloadCV(options.data.fileid);
+            downloadCV(options.data.CVid);
           });
           container.appendChild(downloadButton);
         },
@@ -60,10 +60,10 @@
     });
   });
 
-  async function downloadCV(fileid) {
+  async function downloadCV(CVid) {
     try {
       const response = await fetch(
-        `https://api.recruitly.io/api/cloudfile/download?cloudFileId=${fileid}&apiKey=TEST27306FA00E70A0F94569923CD689CA9BE6CA`
+        `https://api.recruitly.io/api/cloudfile/download?cloudFileId=${CVid}&apiKey=TEST27306FA00E70A0F94569923CD689CA9BE6CA`
       );
 
       if (response.ok) {
