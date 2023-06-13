ca31d0ddea64d959ec43f8272a32fce61fd22f3e
Yugandhar182 <yugandhar@recruitly.io>
2023-06-13 12:09:27 +0530

    .

--- src/App.svelte
+++ src/App.svelte
@@ -60,10 +60,10 @@
     });
   });
 
-  async function downloadCV(id) {
+  async function downloadCV(cvid) {
     try {
       const response = await fetch(
-        `https://api.recruitly.io/api/cloudfile/download?cloudFileId=${id}&apiKey=TEST27306FA00E70A0F94569923CD689CA9BE6CA`
+        `https://api.recruitly.io/api/cloudfile/download?cloudFileId=${cvid}&apiKey=TEST27306FA00E70A0F94569923CD689CA9BE6CA`
       );
 
       if (response.ok) {
