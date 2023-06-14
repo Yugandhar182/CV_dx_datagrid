be51b7ab9731edc8005a58668b562c07c2ef18ed
Yugandhar182 <yugandhar@recruitly.io>
2023-06-14 16:33:50 +0530

    ,

--- src/App.svelte
+++ src/App.svelte
@@ -48,27 +48,43 @@
           });
           container.appendChild(downloadButton);
 
-          const viewButton = document.createElement("button");
-          viewButton.innerText = "View CV";
           viewButton.addEventListener("click", async () => {
-            const cvResponse = await fetch(
-              `https://api.recruitly.io/api/candidatecv/${options.data.id}?apiKey=TEST27306FA00E70A0F94569923CD689CA9BE6CA`
-            );
-            if (cvResponse.ok) {
-              const cvData = await cvResponse.json();
-              const cvHtml = cvData.html;
-              if (cvHtml) {
-                const cvWindow = window.open("", "_blank");
-                cvWindow.document.write(cvHtml);
-                cvWindow.document.close();
-              } else {
-                alert("CV file not found.");
-              }
-            } else {
-              alert("Failed to fetch CV.");
-            }
-          });
-          container.appendChild(viewButton);
+  const cvResponse = await fetch(
+    `https://api.recruitly.io/api/candidatecv/${options.data.id}?apiKey=TEST27306FA00E70A0F94569923CD689CA9BE6CA`
+  );
+  if (cvResponse.ok) {
+    const cvData = await cvResponse.json();
+    const cvHtml = cvData.html;
+    if (cvHtml) {
+      const popupContainer = document.createElement("div");
+      popupContainer.className = "popup-container";
+
+      const popupContent = document.createElement("div");
+      popupContent.className = "popup-content";
+
+      const closeBtn = document.createElement("button");
+      closeBtn.innerText = "Close";
+      closeBtn.className = "close-button";
+      closeBtn.addEventListener("click", () => {
+        popupContainer.remove();
+      });
+
+      const cvWindow = window.open("", "_blank");
+      cvWindow.document.write(cvHtml);
+      cvWindow.document.close();
+
+      popupContent.appendChild(closeBtn);
+      popupContainer.appendChild(popupContent);
+      document.body.appendChild(popupContainer);
+    } else {
+      alert("CV file not found.");
+    }
+  } else {
+    alert("Failed to fetch CV.");
+  }
+  container.appendChild(viewButton);
+});
+
 
           const uploadButton = document.createElement("button");
           uploadButton.innerText = "Upload CV";
@@ -228,24 +244,33 @@
 </script>
 
 <style>
-	.popup-overlay {
-	  position: fixed;
-	  top: 0;
-	  left: 0;
-	  right: 0;
-	  bottom: 0;
-	  background-color: rgba(0, 0, 0, 0.5);
-	  display: flex;
-	  justify-content: center;
-	  align-items: center;
-	}
-  
-	.popup-content {
-	  background-color: white;
-	  padding: 20px;
-	  border-radius: 4px;
-	}
-	</style>
+  .popup-container {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .popup-content {
+    background-color: white;
+    padding: 20px;
+    border-radius: 4px;
+    position: relative;
+  }
+
+  .close-button {
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    cursor: pointer;
+  }
+</style>
+
 
 <h1 style="color: blue;">Job Candidate Details</h1>
 
