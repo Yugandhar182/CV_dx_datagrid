0520e8a09dcd7d5655d43873d81008905645c4de
Yugandhar182 <yugandhar@recruitly.io>
2023-06-14 15:54:29 +0530

    .

--- src/App.svelte
+++ src/App.svelte
@@ -144,10 +144,24 @@
 </script>
 
 <style>
-  #dataGrid {
-    height: 400px;
-  }
-</style>
+	.popup-overlay {
+	  position: fixed;
+	  top: 0;
+	  left: 0;
+	  right: 0;
+	  bottom: 0;
+	  background-color: rgba(0, 0, 0, 0.5);
+	  display: flex;
+	  justify-content: center;
+	  align-items: center;
+	}
+  
+	.popup-content {
+	  background-color: white;
+	  padding: 20px;
+	  border-radius: 4px;
+	}
+	</style>
 
 <h1 style="color: blue;">Job Candidate Details</h1>
 
