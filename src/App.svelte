1e2a2d78fbc84868d0ceda3e5813a76d4570aba7
Yugandhar182 <yugandhar@recruitly.io>
2023-06-14 19:05:27 +0530

    .

--- src/App.svelte
+++ src/App.svelte
@@ -118,10 +118,7 @@ viewButton.addEventListener("click", async () => {
           uploadButton.addEventListener("click", async () => {
             
             isFileSelectionPopupOpen = true;
-            fileInput.addEventListener("change", async (event) => {
-              const file = event.target.files[0];
-              const formData = new FormData();
-              formData.append("file", file);
+           
 
               const uploadResponse = await fetch(
                 `https://api.recruitly.io/api/candidatecv/upload?apiKey=TEST1236C4CF23E6921C41429A6E1D546AC9535E&candidateId=${options.data.id}`,
