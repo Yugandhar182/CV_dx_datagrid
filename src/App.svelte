59911894f7eaa3c97798ecf5e3a4f00f0dea15a8
Yugandhar182 <yugandhar@recruitly.io>
2023-06-14 14:29:36 +0530

    .

--- src/App.svelte
+++ src/App.svelte
@@ -1,40 +1,77 @@
 <script>
-  import { onMount } from "svelte";
-  import "bootstrap/dist/css/bootstrap.min.css";
-  import DevExpress from "devextreme";
-
- 
-
-  let jsonData = [];
-  let gridData = [];
-
- 
-
-  onMount(async () => {
-    const response = await fetch(
-      "https://api.recruitly.io/api/candidate?apiKey=TEST9349C0221517DA4942E39B5DF18C68CDA154"
-    );
-    const responseData = await response.json();
-    jsonData = responseData.data;
-
- 
-
-    gridData = jsonData.map((item) => ({
-      id: item.id,
-      firstName: item.firstName,
-      surname: item.surname,
-      email: item.email,
-      mobile: item.mobile,
-    }));
-
- 
-
-    const columns = [
-      { dataField: "id", caption: "ID", width: 250 },
-      { dataField: "firstName", caption: "Full Name", width: 200 },
-      { dataField: "surname", caption: "Surname", width: 200 },
-      { dataField: "email", caption: "Email", width: 200 },
-      { dataField: "mobile", caption: "Mobile", width: 150 },
+	import { onMount, createEventDispatcher } from "svelte";
+	import "bootstrap/dist/css/bootstrap.min.css";
+	import DevExpress from "devextreme";
+  
+	let jsonData = [];
+	let gridData = [];
+	let isCVUploadPopupVisible = false;
+	let selectedRowData = null;
+	
+	 // Add a variable to store the selected CV identifier
+  
+	async function uploadCV(file) {
+	 
+	  const formData = new FormData();
+	  formData.append("file", file);
+  
+	  if (selectedRowData) {
+		const uploadCandidateId = selectedRowData.id; // Get the candidate ID from selectedRowData
+  
+		try {
+		  const response = await fetch(
+			`https://api.recruitly.io/api/candidatecv/upload?apiKey=TEST1236C4CF23E6921C41429A6E1D546AC9535E&candidateId=${uploadCandidateId}`,
+			{
+			  method: "POST",
+			  body: formData,
+			}
+		  );
+  
+		  if (response.ok) {
+			console.log("CV uploaded successfully!");
+			
+		  } else {
+			console.error("CV upload failed.");
+			
+		  }
+		} catch (error) {
+		  console.error("CV upload error:", error);
+		  
+		}
+	  }
+  
+	  
+	  isCVUploadPopupVisible = false;
+	}
+  const dispatch = createEventDispatcher();
+  
+	onMount(async () => {
+	  const response = await fetch(
+		"https://api.recruitly.io/api/candidate?apiKey=TEST9349C0221517DA4942E39B5DF18C68CDA154"
+	  );
+	  const responseData = await response.json();
+	  jsonData = responseData.data;
+  
+	  gridData = jsonData.map((item) => ({
+		id: item.id,
+		firstName: item.firstName,
+		surname: item.surname,
+		email: item.email,
+		mobile: item.mobile,
+		
+	  }));
+  
+	  const dataGrid = new DevExpress.ui.dxDataGrid(
+		document.getElementById("dataGrid"),
+		{
+		  dataSource: gridData,
+		  columns: [
+			{ dataField: "id", caption: "ID", width: 50 },
+			{ dataField: "firstName", caption: "First Name", width: 200 },
+			{ dataField: "surname", caption: "Surname", width: 200 },
+			{ dataField: "email", caption: "Email", width: 200 },
+			{ dataField: "mobile", caption: "Mobile", width: 150 },
+			
       {
         caption: "Actions",
         width: 250,
@@ -86,64 +123,86 @@
         },
         width: 250,
       },
-      // Add other columns as needed
-    ];
-
- 
-
-    const dataGrid = new DevExpress.ui.dxDataGrid(
-      document.getElementById("dataGrid"),
-      {
-        dataSource: gridData,
-        columns: columns,
-        showBorders: true,
-        filterRow: {
-          visible: true,
-        },
-        editing: {
-          allowDeleting: true,
-          allowAdding: true,
-          allowUpdating: true,
-          mode: "popup",
-          form: {
-            labelLocation: "top",
-          },
-          popup: {
-            showTitle: true,
-            title: "Row in the editing state",
-          },
-          texts: {
-            saveRowChanges: "Save",
-            cancelRowChanges: "Cancel",
-            deleteRow: "Delete",
-            confirmDeleteMessage:
-              "Are you sure you want to delete this record?",
-          },
-        },
-        paging: {
-          pageSize: 10,
-        },
-
- 
-
-        onInitialized: () => {},
-      }
-    );
-  });
-</script>
-
- 
-
-<style>
-  #dataGrid {
-    height: 400px;
-  }
-</style>
-
- 
-
-<h1 style="color: blue;">Job Candidate Details</h1>
-
- 
-
-<div id="dataGrid"></div>
+		  ],
+		  showBorders: true,
+		  filterRow: {
+			visible: true,
+		  },
+		  editing: {
+			allowDeleting: true,
+			allowAdding: true,
+			allowUpdating: true,
+			mode: "popup",
+			form: {
+			  labelLocation: "top",
+			},
+			popup: {
+			  showTitle: true,
+			  title: "Row in the editing state",
+			},
+			texts: {
+			  saveRowChanges: "Save",
+			  cancelRowChanges: "Cancel",
+			  deleteRow: "Delete",
+			  confirmDeleteMessage: "Are you sure you want to delete this record?",
+			},
+			onSaveRowChanges: handleSave, // Bind handleSave function to the saveRowChanges event
+		  },
+		  paging: {
+			pageSize: 20,
+		  },
+		  onRowInserting: async (e) => {
+			// ...
+		  },
+		  onRowUpdating: async (e) => {
+			// ...
+		  },
+		  onRowRemoving: async (e) => {
+			// ...
+		  },
+		  onInitialized: () => {
+			// Function called when the grid is initialized
+			// ...
+		  },
+		}
+	  );
+	});
+	</script>
+  
+	<div id="dataGrid"></div>
+  
+	{#if isCVUploadPopupVisible}
+	<div class="popup-overlay">
+	  <div class="popup-content">
+		<h3>Upload CV</h3>
+		<input
+		  type="file"
+		  on:change="{(event) => uploadCV(event.target.files[0])}"
+		  accept=".pdf,.doc,.docx"
+		/>
+		<button on:click={handleSave} class="btn btn-primary">Save</button>
+		<button on:click={handleClose} class="btn btn-secondary">Close</button>
+	  </div>
+	</div>
+	{/if}
+	
+	<style>
+	.popup-overlay {
+	  position: fixed;
+	  top: 0;
+	  left: 0;
+	  right: 0;
+	  bottom: 0;
+	  background-color: rgba(0, 0, 0, 0.5);
+	  display: flex;
+	  justify-content: center;
+	  align-items: center;
+	}
+  
+	.popup-content {
+	  background-color: white;
+	  padding: 20px;
+	  border-radius: 4px;
+	}
+	</style>
+  
