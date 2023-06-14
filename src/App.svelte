6828042d555a82973656082aff9ca7658606f63a
Yugandhar182 <yugandhar@recruitly.io>
2023-06-14 22:01:53 +0530

    .

--- src/App.svelte
+++ src/App.svelte
@@ -88,7 +88,7 @@
       { dataField: "mobile", caption: "Mobile", width: 150 },
       {
         caption: "Actions",
-        width: 300,
+        width: 500,
         cellTemplate: function (container, options) {
           const downloadButton = document.createElement("button");
           downloadButton.classList.add("btn", "btn-success", "mr-2");
